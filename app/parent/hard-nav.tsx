2487e7726667b1bf8150b962dca1b6e18363c58b
Mike Robinson <mike.robinson@globacore.com>
2023-01-04 09:10:17 -0500

    Annotate navigation links with resulting behaviours

--- app/parent/hard-nav.tsx
+++ app/parent/hard-nav.tsx
@@ -2,10 +2,10 @@ export default function HardNav() {
   return (
     <ul>
       <li>
-        <a href="./child-a">Hard Nav Child A</a>
+        <a href="./child-a">Hard Nav Child A (goes to the right page)</a>
       </li>
       <li>
-        <a href="./child-b">Hard Nav Child B</a>
+        <a href="./child-b">Hard Nav Child B (goes to the right page)</a>
       </li>
     </ul>
   )
--- app/parent/soft-nav.tsx
+++ app/parent/soft-nav.tsx
@@ -4,10 +4,10 @@ export default function SoftNav() {
   return (
     <ul>
       <li>
-        <Link href="./child-a">Soft Nav Child A</Link>
+        <Link href="./child-a">Soft Nav Child A (will 404 unexpectedly)</Link>
       </li>
       <li>
-        <Link href="./child-b">Soft Nav Child B</Link>
+        <Link href="./child-b">Soft Nav Child B (will 404 unexpectedly)</Link>
       </li>
     </ul>
   )
